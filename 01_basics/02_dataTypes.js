5eee2ae691cf5dd8004f2bb61e48c76efb8f81ad
Rohit Roy <133909963+rohitwhore@users.noreply.github.com>
2024-09-13 20:59:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3)  // wrong ayega because we are using nodejs, not browser
+
+let name = "rohit"
+let age = 22
+let isLoggedIn = false
+let state; // undefined
+let s = null; // null
+
+// number => range is 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "rohit");
+console.log(typeof age);
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined
